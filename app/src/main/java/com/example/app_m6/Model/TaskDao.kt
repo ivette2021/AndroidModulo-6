4e7dda298f3126c6d232084158d0a1a1612b7064
ivette2021 <iconstanzav1@gmail.com>
2023-08-05 22:48:47 -0400

    modelo

--- app/src/main/java/com/example/app_m6/Model/TaskDao.kt
+++ app/src/main/java/com/example/app_m6/Model/TaskDao.kt
@@ -1,5 +1,40 @@
 package com.example.app_m6.Model
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
 //INTERFAZ REPRESENTA LAS ACCIONES QUE VAMOS HACER EN NUESTRA BASE DE DATOS
+@Dao
 interface TaskDao {
+    // usamos el @ para realizar una accion ej: @Insert(onConflict = OnConflictStrategy.REPLACE)
+    //ojo que todos se estan trabajando en el mismo hilo no estamos usando corrutinas aqui.
+    //cada dao para cada tabla principio de responsabilidad unica
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertTask(task: Task)
+
+    //insertar multiples tareas
+    @Insert
+    fun insertMultipleTask(list: List<Task>)
+
+    //actualizar
+    @Update
+    fun updateTask(task: Task)
+
+    @Delete
+    fun deleteOneTask(task: Task)
+
+    @Query("SELECT * FROM TASK_TABLE")
+    fun getAlltask1(): List<Task>
+
+    //ejemplo para traer todas las tareas ordenadas por id
+    @Query("SELECT * FROM TASK_TABLE ORDER BY idTask ASC")
+    fun getAlltask(): List<Task>
+
+    //ejemplo de traer solo una tarea
+    @Query("SELECT * FROM TASK_TABLE WHERE idTask = idTask Limit 1")
+    fun getTaskById(idTask: Int): Task
 }
--- app/src/main/java/com/example/app_m6/Model/TaskDataBase.kt
+++ app/src/main/java/com/example/app_m6/Model/TaskDataBase.kt
@@ -1,4 +1,43 @@
 package com.example.app_m6.Model
 
-class TaskDataBase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [Task::class], version=1, exportSchema = false)
+abstract class TaskDataBase : RoomDatabase() {
+
+    //Dao tiene las operaciones de la base de datos
+
+    abstract fun getTaskDao(): TaskDao
+
+
+    //instancia de forma directa
+    companion object {
+
+        @Volatile
+        private var INSTANCE : TaskDataBase?= null
+        fun getDatabase(context: Context): TaskDataBase {               //obtener nuestro contexto
+
+          val tempInstance = INSTANCE
+
+            if (tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){           //sincroniza los hilos en caso de haber mas de uno
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDataBase::class.java,
+                    "task_Database"
+                ).build() //aplicar los cambios
+
+
+                INSTANCE= instance
+                return instance
+            }
+        }
+    }
+
 }
